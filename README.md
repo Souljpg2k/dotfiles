fe34aa852e6a14d3d7649fa42aca251f8e4ec599
arx <crystalsoul2k@gmail.com>
2026-04-29 21:09:22 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# dotfiles
+# Packages
+
+```bash
+sudo pacman -S rofi fastfetch mako code cava neovim kitty starship fish mpv hypridle hyprpicker hyprlock hyprshot btop git nwg-look awww ttf-hack-nerd ttf-mplus-nerd ttf-jetbrains-mono-nerd ttf-baekmuk noto-fonts noto-fonts-emoji
+```
+
+```bash
+yay -S matugen-bin bibata-cursor-theme-bin waypaper-git brave-bin hyprshutdown krabby-bin
+```
---
+++ fish/conf.d/alias.fish
@@ -0,0 +1,23 @@
+# HYPRLAND
+alias h='nvim $HOME/.config/hypr/hyprland.conf'
+alias a='nvim $HOME/.config/hypr/hyprland/animations.conf'
+alias g='nvim $HOME/.config/hypr/hyprland/generals.conf'
+alias i='nvim $HOME/.config/hypr/hyprland/inputs.conf'
+alias k='nvim $HOME/.config/hypr/hyprland/keybinds.conf'
+alias r='nvim $HOME/.config/hypr/hyprland/rules.conf'
+
+# KITTY
+alias kc='nvim $HOME/.config/kitty/kitty.conf'
+
+# FASTFETCH
+alias ff='fastfetch'
+alias ffj='nvim $HOME/.config/fastfetch/config.jsonc'
+
+# ROFI
+alias rr='nvim $HOME/.config/rofi/config.rasi'
+
+# FISH
+alias f='nvim $HOME/.config/fish/config.fish'
+
+# CAVA
+alias c='cava'
